588d91829357f479e91666f7599beb033e523450
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-06-20 11:29:22 +0530

    env file update

--- client/src/component/HorizontalScrollbar.js
+++ client/src/component/HorizontalScrollbar.js
@@ -4,32 +4,32 @@ import {ScrollMenu,VisibilityContext} from 'react-horizontal-scrolling-menu'
 import { Box, Typography } from '@mui/material';
 import BodyPart from './BodyPart'
 import ExerciseCard from './ExerciseCard'
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+// import RightArrowIcon from '../assets/icons/right-arrow.png';
+// import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
+// const LeftArrow = () => {
+//   const { scrollPrev } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow " >
-      <img src={LeftArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
+//   return (
+//     <Typography onClick={() => scrollPrev()} className="right-arrow " >
+//       <img src={LeftArrowIcon} alt="right-arrow" />
+//     </Typography>
+//   );
+// };
 
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
+// const RightArrow = () => {
+//   const { scrollNext } = useContext(VisibilityContext);
 
-  return (
-    <Typography onClick={() => scrollNext()} className="left-arrow " >
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
+//   return (
+//     <Typography onClick={() => scrollNext()} className="left-arrow " >
+//       <img src={RightArrowIcon} alt="right-arrow" />
+//     </Typography>
+//   );
+// };
 
 const HorizontalScrollbar = ({data,bodyPart,setBodyPart,isBodyParts}) => {
   return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} >
+    <ScrollMenu >
       {data.map((item) =>(
       <Box
       key={item.id || item}
